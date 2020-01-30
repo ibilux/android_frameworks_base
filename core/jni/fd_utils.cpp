28302d40b8f8749657be050e2547ed2e2461c8a7
Bilux <i.bilux@gmail.com>
2020-01-30 21:28:01 +0100

    whitelist /proc/ged from zygote

--- core/jni/fd_utils.cpp
+++ core/jni/fd_utils.cpp
@@ -43,6 +43,7 @@ static const char* kPathWhitelist[] = {
   "/dev/ion",
   "/dev/dri/renderD129", // Fixes b/31172436
   "/system/framework/org.lineageos.platform-res.apk",
+  "/proc/ged", //TRONX2100 for MTK_HARDWARE
 };
 
 static const char kFdPath[] = "/proc/self/fd";
